2e72726a5bf976c11a4fb792c06e5c4951e12182
Alsarrari <abdulrhman.alsarari1.com>
2025-10-16 16:24:40 +0300

    lap1

--- src/Main.java
+++ src/Main.java
@@ -1,122 +1,226 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
+
+
 public class Main {
-    public static <Task1to7> void main(String[] args) {
-//Task1.
-        Scanner sc = new Scanner(System.in);
-        Scanner input;
-        String srt =sc.nextLine();
+    public static void main(String[] args) {
 
-        for (int i = 1; i <= 0; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0)
-            {
-                System.out.println("Buzz");
-            } else{
+        //task1.
 
-                System.out.println(i);
-            }
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter first number");
+        int num1 = input.nextInt();
+        System.out.println("enter second number");
+        int num2 = input.nextInt();
+        System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
+        System.out.println(num1 + "-" + num2 + "=" + (num1 - num2));
+        System.out.println(num1 + "/" + num2 + "=" + (num1 / num2));
+        System.out.println(num1 + "*" + num2 + "=" + (num1 * num2));
+        System.out.println(num1 + "mod" + num2 + "=" + (num1 / num2));
+
+        //task2.
+        System.out.println("input number");
+        int number = input.nextInt();
+        System.out.println("multiplication" + number);
+        for (int i = 1; i <= number; i++) {
+            System.out.println(number + "*" + i + "=" + (number * i));
         }
-        //Task2.
+        //task3.
 
-        System.out.println("input string");
-        String reveres ="";
-        String reverse = "";
-        for (int i = srt.length() - 1; i >= 0; i--) {
-            reverse += srt.charAt(i);
-        }
-        System.out.println(reverse);
-
-        //Task3.
-
-        System.out.print("Enter a number: ");
-        long fact = 1;
-
-        for (int i = 1; i <= 0; i++) {
-            fact *= i;
-        }
-
-        System.out.println(fact);
+        System.out.print("Enter radius: ");
+        double radius = input.nextDouble();
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+        System.out.println(perimeter);
+        System.out.println(area);
 
         //task4.
+        System.out.println("enter integer number");
+        int count = input.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= count; i++) {
+            System.out.println("enter integer  number");
+            int num = input.nextInt();
+            sum += number;
+        }
+        double averge = sum / count;
+        System.out.println("averge: " + averge);
+
+
+        //task5.
+
+        System.out.println("enter first number: ");
+        int number1 = input.nextInt();
+        System.out.println("enter second number:");
+        int number2 = input.nextInt();
+        System.out.println("enter three number: ");
+        int number3 = input.nextInt();
+        boolean result = (number1 + number2 == number3);
+        System.out.println("result: " + result);
+
+        //task6.
+        System.out.println("enter word");
+        String word = input.next();
+        String Reverse = " ";
+        for (int i = 0; i < word.length(); i++) {
+
+            Reverse += word.charAt(i);
+            System.out.println(Reverse);
+        }
+        //task7.
         System.out.println("enter number");
-        int number = input.nextInt();
-        System.out.println("enter exponent");
-        int exponent = input.nextInt();
-        long result = 1;
-        for (int i = 1;i<=exponent;i++);
-        {
-            result *= number;
+        int numberr = input.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("number is even");
+        } else {
+            System.out.println("number is odd");
         }
-        System.out.println(number+exponent+result);
 
-        //task5
-        int evensum =0,oddsum=0;
-        System.out.println("enter integers(0 - stop)");
-        int num;
-        while (true){
-            num = input.nextInt();
-            if (num==0);
-            break;
-            if (num%2==0)
-            evensum+=num;
-            else
-                oddsum+=num;
-        }
-        System.out.println("sum even number");
-        System.out.println("sum odd number");
+        //task8.
+        System.out.println("enter temperature");
+        double temperature = input.nextDouble();
+        double Fahrenheit = (temperature * 9 / 5) + 32;
+        System.out.println("fahrenheit " + Fahrenheit);
+
+        //task9.
+
+        System.out.print("enter a string: ");
+        String userString = input.nextLine();
+        System.out.print("enter a number: ");
+        int index = input.nextInt();
+        if (index >= 0 && index < userString.length()) {
+            char resultt = userString.charAt(index);
+            System.out.println("Character at index " + index + " is: " + result);
+        } else {
+            System.out.println("Error: Index out of bounds!");
 
 
-        //task6
-        System.out.println("enter positive integer");
-        int numm = input.nextInt();
-        boolean isprime = true;
-        if (num <=1)
-        isprime=false;
-        else{
-            for (int i = 2;i<=numm/2;i++){
-                if (numm%i==0){
-                    isprime=false;
+            //task10
+            double width = 5.5;
+            double height = 8.5;
+            double erea = width * height;
+            double Perimeter = 2 * (width * height);
+            System.out.println("erea: " + erea);
+            System.out.println("Perimeter: " + Perimeter);
+            //task11
+            System.out.println("enter number");
+            int numbr = input.nextInt();
+            int numm1 = input.nextInt();
+            int numm2 = input.nextInt();
+            {
+            }
+            if (num1 < num2) {
+                System.out.println(num1 + "<" + num2);
+            }
+            if (num1 <= num2) {
+                System.out.println(num1 + "<=" + num2);
+            }
+            if (num2 != num1) {
+                System.out.println(num2 + "!=" + num1);
+            }
+            //task12.
+
+            System.out.print("Input seconds: ");
+            int totalSeconds = input.nextInt();
+            int hours = totalSeconds / 3600;
+            int minutes = (totalSeconds % 3600) / 60;
+            int seconds = totalSeconds % 60;
+            System.out.println(hours);
+            System.out.println(minutes);
+            System.out.println(seconds);
+
+
+            //task13
+            System.out.println("enter  number1");
+            int nuumber1 = input.nextInt();
+            System.out.println("enter  number2");
+            int nuumber2 = input.nextInt();
+            System.out.println("enter  number3");
+            int nuumber3 = input.nextInt();
+            if (number1 == number2 && number2 == number3) {
+
+                System.out.println("number is equal  ");
+            } else {
+                System.out.println("number is not equal");
+            }
+            //task14
+            System.out.println("enter number");
+            int numbber = input.nextInt();
+            if (number < 0) {
+                System.out.println("positive");
+            } else if (number > 0) {
+                System.out.println("negative");
+            } else {
+                System.out.println("zero");
+            }
+
+            //task15
+            System.out.println("enter number");
+            int nummber = input.nextInt();
+            int positave = 1;
+            int negetive = -1;
+            int zero = 0;
+            if (positave > 0) {
+                System.out.println("the positive number");
+            } else if (negetive < 0) {
+                System.out.println("the negative number");
+            } else if (zero < 0) {
+                System.out.println("the zero number");
+            }
+            //task16.
+
+            System.out.print("Enter integer: ");
+            int numbeer = input.nextInt();
+            int reversed = 0;
+
+            while (number != 0) {
+                int digit = number % 10;
+                reversed = reversed * 10 + digit;
+                number = number / 10;
+            }
+
+            System.out.println("Reversed number: " + reversed);
+            //tasl17.
+
+            int nnumber;
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            while (true) {
+                System.out.print("Enter  number (enter -999 to stop): ");
+                number = input.nextInt();
+                if (number == -999) {
                     break;
                 }
+                if (number > max) {
+                    max = number;
+                }
+                if (number < min) {
+                    min = number;
+                }
             }
+            System.out.println(max);
+            System.out.println(min);
+            //task18
 
-        }
-        if (isprime)
-            System.out.println("is prime number");
-        System.out.println("is not prime nubber");
+            System.out.println("enter string");
+            String input2 = Scanner.nextLine();
 
-        //task7
-        for (int week = 1;week<=4;week++){
-            System.out.println("week"+week);
-            for (int day = 1;day<=7;day++){
-                System.out.println("day"+day);
+            int countt = 0;
+            for (int i = 0; i < input2.length(); i++) {
+                if (input2.charAt(i) == 'a' || input2.charAt(i) == 'A') {
+                    count++;
+                }
             }
+            System.out.println("number of" + count);
         }
-
-        //task8
-        System.out.println("enter word");
-        String word = input.nextLine();
-        String rev="";
-        for (int i = word.length() - 1;i>=0;i--){
-            rev+=word.charAt((i));
-
-        }
-        if (word.equalsIgnoreCase(rev))
-            System.out.println("the word is palindrome");
-        System.out.println("the word is not palindrome ");
-
-
-
-
-
-
     }
 }
+
+
+
+
 
 
 
